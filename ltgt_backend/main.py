3640b8cb760ac0021b3b2f71f2a4e545ab54db9d
SidNamo <haheehong3@naver.com>
2024-08-05 16:11:51 +0900

    webrtc 샘플

--- ltgt_backend/main.py
+++ ltgt_backend/main.py
@@ -1,127 +1,48 @@
-# -*- coding: utf-8 -*-
-import logging
-import multiprocessing
 import os
-import time
-import wave
-from multiprocessing import set_start_method
-from multiprocessing.queues import Queue
-from typing import Optional
 
-import uvicorn
-from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status, Request
+from fastapi import FastAPI, staticfiles, Request
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import FileResponse
+from fastapi.websockets import WebSocket, WebSocketDisconnect
+from signaling import MeetingManager
 
-format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.DEBUG,
-                    datefmt="%H:%M:%S")
 
 app = FastAPI()
 
 root = os.path.dirname(__file__)
-
 app.mount('/static', StaticFiles(directory=os.path.join(root, 'static')), name='static')
+templates = Jinja2Templates(directory="templates")
 
-templates = Jinja2Templates(directory=os.path.join(root, 'templates'))
+meeting_manager = MeetingManager()
 
-
-@app.get('/favicon.ico')
-async def get():
-    return FileResponse(os.path.join(root, 'static', 'src', 'favicon.ico'))
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-async def get(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+def home(): # type: ignore
+    return RedirectResponse("/lobby")
 
+@app.get("/room/{roomName}")
+def read_root(request: Request, roomName:str):
+    return templates.TemplateResponse(request=request, name="index.html")
 
-async def get_cookie_or_token(
-        websocket: WebSocket,
-        session: Optional[str] = Cookie(None),
-        token: Optional[str] = Query(None),
-):
-    if session is None and token is None:
-        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
-    return session or token
+@app.get("/lobby")
+def get_lobby(request: Request):
+    return templates.TemplateResponse(request=request, name="lobby.html")
 
-
-def wav_worker(q: Queue, uid: str, ):
-    root = os.path.join(os.path.dirname(__file__), 'upload_waves')
-    os.makedirs(root, exist_ok=True)
-    filename = os.path.join(root, f'{uid}_{time.time()}.wav')
-    try:
-        wav = wave.open(filename, mode='wb')
-        wav.setframerate(16000)
-        wav.setnchannels(1)
-        wav.setsampwidth(2)
-
-        while True:
-            data_bytes = q.get()
-            wav.writeframes(data_bytes)
-            print(f'q.get {len(data_bytes)}')
-
-    except Exception as e:
-        logging.debug(e)
-    finally:
-        wav.close()
-
-    logging.info('leave wav_worker')
-
-
-@app.websocket("/items/{item_id}/ws")
-async def websocket_endpoint(
-        websocket: WebSocket,
-        item_id: str,
-        q: Optional[int] = None,
-        cookie_or_token: str = Depends(get_cookie_or_token),
-):
-    await websocket.accept()
-    logging.info('websocket.accept')
-
-    ctx = multiprocessing.get_context()
-    queue = ctx.Queue()
-    process = ctx.Process(target=wav_worker, args=(queue, item_id))
-    process.start()
-    counter = 0
-
+@app.websocket("/ws/{client_id}")
+async def connet_websocket(websocket: WebSocket, client_id: str):
+    await meeting_manager.join(client_id, websocket)
     try:
         while True:
-            data_bytes = await websocket.receive_bytes()
-            data = [int.from_bytes(data_bytes[i:i + 2], byteorder='little', signed=True) for i in
-                    range(0, len(data_bytes), 2)]
-            await websocket.send_text(
-                f"Session cookie or query token value is: {cookie_or_token}. counter {counter}"
-            )
-            if q is not None:
-                await websocket.send_text(f"Query parameter q is: {q}")
-            # await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
-
-            queue.put(data_bytes)
-            counter += 1
-
-    except Exception as e:
-        logging.debug(e)
-    finally:
-        # Wait for the worker to finish
-        queue.close()
-        queue.join_thread()
-        # use terminate so the while True loop in process will exit
-        process.terminate()
-        process.join()
-
-    logging.info('leave websocket_endpoint')
-
-
-if __name__ == '__main__':
-    # When using spawn you should guard the part that launches the job in if __name__ == '__main__':.
-    # `set_start_method` should also go there, and everything will run fine.
-    try:
-        set_start_method('spawn')
-    except RuntimeError as e:
-        print(e)
-
-    uvicorn.run('main:app', reload=True, log_level='debug',
-                ssl_keyfile=os.path.join(root, 'static', 'key.pem'),
-                ssl_certfile=os.path.join(root, 'static', 'cert.pem'))
+            data = await websocket.receive_json()
+            await meeting_manager.rooms[client_id].broadcast(data, websocket)
+    except WebSocketDisconnect:
+        meeting_manager.leave(client_id, websocket)
